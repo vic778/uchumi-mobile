aa03956d4ed5c5afde6f1cd7b55fbbc2ded8fcce
vic778 <victoiremmanuelbarh@gmail.com>
2026-09-26 13:32:29 +0200

    Add Fonts design token (Host Grotesk)

---
+++ src/constants/fonts.ts
@@ -0,0 +1,20 @@
+import {
+  HostGrotesk_400Regular,
+  HostGrotesk_500Medium,
+  HostGrotesk_600SemiBold,
+  HostGrotesk_700Bold,
+} from '@expo-google-fonts/host-grotesk';
+
+export const fontAssets = {
+  HostGrotesk_400Regular,
+  HostGrotesk_500Medium,
+  HostGrotesk_600SemiBold,
+  HostGrotesk_700Bold,
+};
+
+export const Fonts = {
+  regular:  'HostGrotesk_400Regular',
+  medium:   'HostGrotesk_500Medium',
+  semiBold: 'HostGrotesk_600SemiBold',
+  bold:     'HostGrotesk_700Bold',
+} as const;
