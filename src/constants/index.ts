1ce0562fd7d3a5669c744e6e413c6f3aeadc4033
vic778 <victoiremmanuelbarh@gmail.com>
2026-09-26 13:32:29 +0200

    Add constants barrel export

---
+++ src/constants/index.ts
@@ -0,0 +1,7 @@
+export * from './colors';
+export * from './fonts';
+export * from './spacing';
+export * from './layout';
+export * from './typography';
+export * from './routes';
+export * from './api';
