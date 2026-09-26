bf55798aead17c14a3cf1ef559922ee9d0a002cb
vic778 <victoiremmanuelbarh@gmail.com>
2026-09-26 13:32:37 +0200

    Add member account service

---
+++ src/services/member/account.ts
@@ -0,0 +1,17 @@
+import { api } from '../api';
+import type { Account, Transaction, Notification } from '@/types';
+
+export async function getAccount(): Promise<Account> {
+  const res = await api.get<{ data: Account }>('/member/account');
+  return res.data.data;
+}
+
+export async function getTransactions(): Promise<Transaction[]> {
+  const res = await api.get<{ data: Transaction[] }>('/member/transactions');
+  return res.data.data;
+}
+
+export async function getNotifications(): Promise<Notification[]> {
+  const res = await api.get<{ data: Notification[] }>('/member/notifications');
+  return res.data.data;
+}
